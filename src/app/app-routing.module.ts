67c83dbebc85022a011c8ba4f7501f1eea862c44
Jesus Jo <32987141+jesusj0o@users.noreply.github.com>
2022-03-09 18:06:38 -0700

    Base design added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//Components
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'list-employee', pathMatch: 'full'}, 
+  {path: 'list-employee', component: ListEmployeeComponent},
+  {path:'create-employee', component: CreateEmployeeComponent}, 
+  {path: '**', redirectTo: 'list-employee', pathMatch: "full"}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    ListEmployeeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
